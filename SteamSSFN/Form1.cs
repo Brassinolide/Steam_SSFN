fb2f0a3b89a6daae914ac9cda3a001249e045acd
渟雲 <QuQ@outlook.it>
2023-02-03 23:34:53 +0800

    need not setting manually

--- SteamSSFN/Form1.cs
+++ SteamSSFN/Form1.cs
@@ -157,7 +157,7 @@ namespace SteamSSFN
                         File.Move("temp", textBox2.Text + "/" + loginssfn);
                         if (checkBox1.Checked == true)
                         {
-                            System.Diagnostics.Process.Start(textBox2.Text + "/steam.exe", "-login " + login +" "+ password+ " -noreactlogin -rememberpassword -windowed ");
+                            System.Diagnostics.Process.Start(textBox2.Text + "/steam.exe", "-login " + login +" "+ password+ " -noreactlogin -rememberpassword -windowed -bigpicture ");
                         }
                         MessageBox.Show("一键上号成功");
                     }
