67d7ad1a2afe1df37f41ae93ce4e684e06bcf2d1
Brassinolide <99178854+Brassinolide@users.noreply.github.com>
2024-04-03 17:28:18 +0800

    fix

--- SteamSSFN/Form1.cs
+++ SteamSSFN/Form1.cs
@@ -1,11 +1,9 @@
 ﻿using Microsoft.Win32;
 using System;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Windows.Forms;
-using static System.Net.WebRequestMethods;
 
 namespace SteamSSFN
 {
@@ -50,6 +48,52 @@ namespace SteamSSFN
             return (string)key.GetValue("SteamPath"); ;
         }
 
+        //写入授权
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (File.Exists("temp")) File.Delete("temp");
+            //获取ssfn下载地址
+            //HttpDownloadFile("http://124.222.242.85/home/ssfn/" + textBox1.Text, "temp");//抓包找到的大D的数据库
+            HttpDownloadFile("https://ssfnbox.com/download/" + textBox1.Text, "temp");
+            string ssfnURL = GetBetweenStr(File.ReadAllLines("temp")[12], "window.location.assign(\"", "\"");
+            if (string.IsNullOrEmpty(ssfnURL))
+            {
+                MessageBox.Show("未找到SSFN", "错误");
+            }
+            else
+            {
+                //判断steam目录是否正确
+                if (File.Exists(textBox2.Text + "/steam.exe"))
+                {
+                    //在steam根目录下搜索并删除ssfn
+                    string[] searchfile = Directory.GetFiles(textBox2.Text, "ssfn*").Select(path => Path.GetFileName(path)).ToArray();
+
+                    foreach (string ssfn in searchfile)
+                    {
+                        if (File.Exists(textBox2.Text + "\\" + ssfn))
+                        {
+                            File.Delete(textBox2.Text + "\\" + ssfn);
+                        }
+                    }
+
+                    //下载ssfn
+                    HttpDownloadFile("https://ssfnbox.com" + ssfnURL, "temp");
+                    //移动至steam根目录
+                    File.Move("temp", textBox2.Text + "/" + textBox1.Text);
+                    //启动steam
+                    if (checkBox1.Checked == true)
+                    {
+                        System.Diagnostics.Process.Start(textBox2.Text, "-noreactlogin");
+                    }
+                    MessageBox.Show("SSFN写入成功");
+                }
+                else
+                {
+                    MessageBox.Show("您确定您选择的Steam路径准确无误？", "错误");
+                }
+            }
+        }
+
         //启动时自动获取steam路径
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -76,30 +120,24 @@ namespace SteamSSFN
             }
             textBox2.Text = FolderPath;
         }
-        {
-            System.Diagnostics.Process myproc = new System.Diagnostics.Process();
-            //得到所有打开的进程   
-            try
-            {
-                foreach (Process thisproc in Process.GetProcessesByName("steam.exe"))
-                {
-                    //找到程序进程,kill之。
-                    if (!thisproc.CloseMainWindow())
-                    {
-                        thisproc.Kill();
-                    }
-                }
 
-            }
-            catch (Exception Exc)
+        //运行steam
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(textBox2.Text + "/steam.exe"))
             {
-                MessageBox.Show(Exc.Message);
+                System.Diagnostics.Process.Start(textBox2.Text + "/steam.exe", "-noreactlogin");
+            }
+            else
+            {
+                MessageBox.Show("您确定您选择的Steam路径准确无误？", "错误");
             }
         }
 
         //一键上号
         private void button1_Click_1(object sender, EventArgs e)
         {
+            if (File.Exists("temp")) File.Delete("temp");
             //分割字符串
             string key = textBox3.Text;
             string[] keyarr = key.Split(new string[] { "----" }, StringSplitOptions.None);
@@ -108,6 +146,9 @@ namespace SteamSSFN
                 string login = keyarr[0].ToString();
                 string password = keyarr[1].ToString();
                 string loginssfn = keyarr[2].ToString();
+                //获取ssfn下载地址
+                HttpDownloadFile("https://ssfnbox.com/download/" + loginssfn, "temp");
+                string ssfnURL = GetBetweenStr(File.ReadAllLines("temp")[12], "window.location.assign(\"", "\"");
                 if (string.IsNullOrEmpty(ssfnURL))
                 {
                     MessageBox.Show("未找到SSFN", "错误");
@@ -115,15 +156,27 @@ namespace SteamSSFN
                 else
                 {
                     //判断steam目录是否正确
+                    if (File.Exists(textBox2.Text + "/steam.exe"))
+                    {
                         //在steam根目录下搜索并删除ssfn
                         string[] searchfile = Directory.GetFiles(textBox2.Text, "ssfn*").Select(path => Path.GetFileName(path)).ToArray();
 
                         foreach (string ssfn in searchfile)
                         {
-                            if (System.IO.File.Exists(textBox2.Text + "\\" + ssfn))
+                            if (File.Exists(textBox2.Text + "\\" + ssfn))
                             {
-                                System.IO.File.Delete(textBox2.Text + "\\" + ssfn);
+                                File.Delete(textBox2.Text + "\\" + ssfn);
                             }
+                        }
+
+                        //下载ssfn
+                        HttpDownloadFile("https://ssfnbox.com" + ssfnURL, "temp");
+                        //移动至steam根目录
+                        File.Move("temp", textBox2.Text + "/" + loginssfn);
+                        //启动steam
+                        if (checkBox1.Checked == true)
+                        {
+                            System.Diagnostics.Process.Start(textBox2.Text + "/steam.exe", "-login " + login + " " + password + " -noreactlogin -rememberpassword -windowed -bigpicture ");
                         }
                         MessageBox.Show("一键上号成功");
                     }
